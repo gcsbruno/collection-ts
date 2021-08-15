2de761eb48f53fba24cc1aa8cf0d1e8b2392907b
gcsbruno <brunoguimaraes.cs@gmail.com>
2021-08-15 02:26:38 -0300

    segundo commit

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-
-import {Title} from './styles'
+import { FiChevronRight } from 'react-icons/fi'
+import {Title, Form, Repos } from './styles'
+import logo  from '../../assets/logo.svg';
 
 export const Dashboard: React.FC = () => {
     return (
-        <Title> Dashboard</Title>
+        <>
+        <img src={logo} alt="GitCollection" />
+        <Title> Catálogo de repositórios do GitHub </Title>
+
+        <Form>
+            <input placeholder="username/repository-name" />
+            <button type="submit">Buscar</button>
+        </Form>
+
+        <Repos>
+            <a href="/repositories">
+                <img src="https://avatars.githubusercontent.com/u/72214347?v=4" alt="Repository" />
+                <div>
+                    <strong>gcsbruno/collection-ts</strong>
+                    <p>Repositório do projeto em typescript</p>
+                </div>
+                <FiChevronRight size={20} />
+            </a>
+        </Repos>
+
+        </>
     )
 }
