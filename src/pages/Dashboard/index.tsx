067b716ac65fa96bda3c1804df0597cc64663a74
gcsbruno <brunoguimaraes.cs@gmail.com>
2021-08-17 05:19:42 -0300

    dashboard completo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import { FiChevronRight } from 'react-icons/fi'
-
+import { Link } from 'react-router-dom'
 import { api } from '../../services/api'
-import {Title, Form, Repos, Error } from './styles'
-import logo  from '../../assets/logo.svg';
+import { Title, Form, Repos, Error } from './styles'
+import logo from '../../assets/logo.svg';
 
 
 interface GithubRepository {
-    full_name: string;
-    description: string;
-    owner: {
-        login: string;
-        avatar_url: string;
-    };
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
 
 }
 export const Dashboard: React.FC = () => {
-    const [repos, setRepos] = React.useState<GithubRepository[]>(() =>{
-      const storageRepos = localStorage.getItem('@GitCollection:repositories');
+  const [repos, setRepos] = React.useState<GithubRepository[]>(() => {
+    const storageRepos = localStorage.getItem('@GitCollection:repositories');
 
-      if (storageRepos) {
-        return JSON.parse(storageRepos)
-      }
+    if (storageRepos) {
+      return JSON.parse(storageRepos)
+    }
 
-      return [];
-    });
-    const [newRepo, setNewRepo] = React.useState('');
-    const [inputError, setInputError] = React.useState('');
+    return [];
+  });
+  const [newRepo, setNewRepo] = React.useState('');
+  const [inputError, setInputError] = React.useState('');
+  const formEl = React.useRef<HTMLFormElement | null>(null);
 
-    React.useEffect(() => {
-      localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
-    }, [repos])
+  React.useEffect(() => {
+    localStorage.setItem('@GitCollection:repositories', JSON.stringify(repos));
+  }, [repos])
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>): void {
     setNewRepo(event.target.value);
@@ -41,33 +42,39 @@ export const Dashboard: React.FC = () => {
   ): Promise<void> {
     event.preventDefault();
 
-    if(!newRepo) {
+    if (!newRepo) {
       setInputError('Informe o username/repositório')
       return
     }
-    const response = await api.get<GithubRepository>(`repos/${newRepo}`);
+    try {
+      const response = await api.get<GithubRepository>(`repos/${newRepo}`);
 
-    const repository = response.data;
+      const repository = response.data;
 
-    setRepos([...repos, repository]);
-    setNewRepo('');
+      setRepos([...repos, repository]);
+      formEl.current?.reset();
+      setNewRepo('');
+      setInputError('')
+    } catch {
+      setInputError('Repositório não encontrado no github')
+    }
   }
 
-    return (
-        <>
-        <img src={logo} alt="GitCollection" />
-        <Title> Catálogo de repositórios do GitHub </Title>
+  return (
+    <>
+      <img src={logo} alt="GitCollection" />
+      <Title> Catálogo de repositórios do GitHub </Title>
 
-        <Form hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
-            <input placeholder="username/repository_name" onChange={handleInputChange} />
-            <button type="submit">Buscar</button>
-        </Form>
+      <Form ref={formEl} hasError={Boolean(inputError)} onSubmit={handleAddRepo}>
+        <input placeholder="username/repository_name" onChange={handleInputChange} />
+        <button type="submit">Buscar</button>
+      </Form>
 
-        {inputError && <Error>{inputError}</Error>}
+      {inputError && <Error>{inputError}</Error>}
 
-        <Repos>
-        {repos.map(repository => (
-          <a href="/repositories" key={repository.full_name}>
+      <Repos>
+        {repos.map((repository, index) => (
+          <Link to={`/repositories/${repository.full_name + index}`} key={repository.full_name}>
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -77,10 +84,10 @@ export const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
-        </Repos>
+      </Repos>
 
-        </>
-    )
+    </>
+  )
 }
